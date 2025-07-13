76fcbcde6a96540577181995a0a659888087265a
Elham9494 <Elhamgdpn.dent@gmail.com>
2025-07-13 09:18:58 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>🦷 دندانپزشکی برای همه</h1>
+      <p>به وبلاگ ساده و مفید دندانپزشکی خوش آمدید!</p>
+      <p>اگه دندون‌درد داری و دسترسی به دندانپزشک نداری، من اینجام که کمکت کنم 💬</p>
+    </div>
+  );
+}
